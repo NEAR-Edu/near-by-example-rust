77d7676346fff1c630cc7bc7b88b1fce1428b7ff
Ryan <ryan.mehta@gmail.com>
2021-06-23 09:36:48 -0700

    update includeFiles

--- pages/api/exercises/[id]/index.js
+++ pages/api/exercises/[id]/index.js
@@ -1,21 +1,19 @@
 import { readFileSync } from "fs";
 import { join } from "path";
 
-const BASH_PATH = process.env.LOCAL ? process.cwd() : __dirname;
-
 export default function handler(req, res) {
   const { id } = req.query;
 
   const explaination = readFileSync(
-    join(BASH_PATH, "exercises", id, "README.md"),
+    join(process.cwd(), "exercises", id, "README.md"),
     "utf-8"
   );
   const starterCode = readFileSync(
-    join(BASH_PATH, "exercises", id, "lib.rs"),
+    join(process.cwd(), "exercises", id, "lib.rs"),
     "utf-8"
   );
   const testCode = readFileSync(
-    join(BASH_PATH, "exercises", id, "test.rs"),
+    join(process.cwd(), "exercises", id, "test.rs"),
     "utf-8"
   );
 
